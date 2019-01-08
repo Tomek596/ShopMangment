a3fe187dc7174cbaa0cddfac0b4b012b4b8af693
Tomasz Poskonka <tomekp33@gmail.com>
2019-01-08 10:57:17 +0100

    init commit and created models

---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private int id;
+    private String login;
+    private String password;
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+}
